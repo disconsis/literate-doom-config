8204b9359597ec52b720bf382afd7c64040bcf2e
disconsis <ketan.kanishka@nyu.edu>
2020-06-13 10:14:21 +0530

    remove evil-escape

--- packages.el
+++ packages.el
@@ -1,6 +1,9 @@
 ;; restore substitute behaviour with s/S
 (package! evil-snipe :disable t)
 
+;; C-[ works well enough for me
+(package! evil-escape :disable t)
+
 ;; syntax-highlighted diffs
 (package! magit-delta)
 
