d60cec33181541dcabf385120f076bd716a46130
disconsis <ketan.kanishka@nyu.edu>
2020-06-18 13:28:09 +0530

    color `pd-easy-annot` mode-line indicator

--- local/pdf-easy-annot/pdf-easy-annot.el
+++ local/pdf-easy-annot/pdf-easy-annot.el
@@ -12,7 +12,9 @@
 ;; (underline, strikethrough.) but I never use those so I couldn't find the
 ;;
 ;; TODO Color the mode line indicator with the highlighting color
+;; TODO Maybe color the bg of the indicator in the color of the pdf
 ;; TODO Add color buttons to mode line to quickly switch highlight colors
+;; TODO Auto-add text annots on click (as opposed to selection - which highlights)
 ;;
 ;; Code:
 
@@ -21,10 +23,31 @@
 (defun pdf-easy-annot/add-hl-advice (&rest _)
   (call-interactively #'pdf-annot-add-highlight-markup-annotation))
 
+
+;; Possible indicators for other modes:
+;;   ﲋ
 (defvar pdf-easy-annot-hl-mode-line-indicator "ﭑ"
   "Mode line indicator for `pdf-easy-annot-hl-minor-mode'.
 Must be set *before* turning on `pdf-easy-annot-hl-minor-mode'.
 Set to `nil' to disable.")
+
+
+(defun pdf-easy-annot/hl-mode-line-color ()
+  "Foreground color to use for mode line indicator.
+TODO This *should* be the color that will be used for the next highlighting.
+Right now it's just a hack, since 'yellow' is the default highlight color."
+
+  "yellow")
+
+
+(defun pdf-easy-annot/hl-mode-line-info ()
+  "Mode line sexp built from `pdf-easy-annot-hl-mode-line-indicator'.
+Uses the foreground color which will be used for the next highlighting.
+TODO check if the indicator is displayable, otherwise use a default ('HL' ?)"
+
+  (let ((fg-color (pdf-easy-annot/hl-mode-line-color)))
+    (propertize pdf-easy-annot-hl-mode-line-indicator 'font-lock-face
+                `(:foreground ,fg-color))))
 
 
 ;;;###autoload
@@ -37,14 +60,17 @@ Set to `nil' to disable.")
       (turn-on-pdf-easy-annot-hl-minor-mode)
     (turn-off-pdf-easy-annot-hl-minor-mode)))
 
+
 ;;;###autoload
 (defun turn-on-pdf-easy-annot-hl-minor-mode ()
   (interactive)
   (advice-add #'pdf-view-mouse-set-region :after #'pdf-easy-annot/add-hl-advice)
   (when pdf-easy-annot-hl-mode-line-indicator
-    (push `(pdf-easy-annot-hl-minor-mode
-            (" " ,pdf-easy-annot-hl-mode-line-indicator " "))
-          mode-line-misc-info)))
+    ;; Don't need to remove from mode-line-misc-info since we use `pushnew' to add it.
+    (pushnew '(pdf-easy-annot-hl-minor-mode
+               (:eval (pdf-easy-annot/hl-mode-line-info)))
+             mode-line-misc-info)))
+
 
 (defun turn-off-pdf-easy-annot-hl-minor-mode ()
   (interactive)
