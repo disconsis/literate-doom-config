71aabfb0749814079f28281a51873d264ded7de5
disconsis <ketan.kanishka@nyu.edu>
2020-06-18 13:46:43 +0530

    add autoload file for pdf-easy-annot

---
+++ local/pdf-easy-annot/autoload.el
@@ -0,0 +1,33 @@
+;;; autoload.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "pdf-easy-annot" "pdf-easy-annot.el" (24298
+;;;;;;  22045 421673 473000))
+;;; Generated autoloads from pdf-easy-annot.el
+
+(autoload 'pdf-easy-annot-hl-minor-mode "pdf-easy-annot" "\
+A minor mode to automatically highlight text on selection.
+
+If called interactively, enable Pdf-Easy-Annot-Hl minor mode if
+ARG is positive, and disable it if ARG is zero or negative.  If
+called from Lisp, also enable the mode if ARG is omitted or nil,
+and toggle it if ARG is `toggle'; disable the mode otherwise.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'turn-on-pdf-easy-annot-hl-minor-mode "pdf-easy-annot" nil t nil)
+
+(autoload 'turn-off-pdf-easy-annot-hl-minor-mode "pdf-easy-annot" nil t nil)
+
+;;;***
+
+(provide 'autoload)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; autoload.el ends here
