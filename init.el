e48f7fc38f2c736be22b3b325a2271849dc73038
disconsis <ketan.kanishka@nyu.edu>
2020-06-27 22:46:11 +0530

    enable nix, ocaml, parinfer

--- init.el
+++ init.el
@@ -45,7 +45,7 @@
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       ;;parinfer          ; turn lisp into python, sort of
+       parinfer            ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
        snippets            ; my elves. They type so I don't have to
        word-wrap           ; soft wrapping with language-aware indent
@@ -126,8 +126,8 @@
        ;;lua               ; one-based indices? one-based indices
        (markdown +grip)    ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
-       ;;ocaml             ; an objective camel
+       nix                 ; I hereby declare "nix geht mehr!"
+       ocaml               ; an objective camel
        (org +roam +noter)  ; organize your plain life in plain text
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
