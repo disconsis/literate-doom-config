20269d740bfe1ce60a4de3e3070dece10eb2c494
disconsis <ketan.kanishka@nyu.edu>
2020-06-28 16:27:35 +0530

    add a fuckton of themes

--- packages.el
+++ packages.el
@@ -21,3 +21,42 @@
 
 ;; utility package
 (package! move-text)
+
+
+;; ------------
+;;    themes
+;; ------------
+
+;; packs a number of themes: https://github.com/owainlewis/emacs-color-themes
+;; - granger
+;; - junio
+;; - mccarthy
+;; - fogus
+(package! sublime-themes)
+
+;; famed for great org-specific faces
+(package! leuven-theme)
+
+(package! ir-black-theme)
+(package! clues-theme)
+
+;; highlights by changing bg color of some faces
+(package! soothe-theme)
+
+;; fun grey-ish theme, great light theme => sorta like zenburn
+(package! apropospriate-theme)
+
+;; pair of high-contrast themes
+(package! modus-operandi-theme)
+(package! modus-vivendi-theme)
+
+;; the fabled zenburn
+(package! zenburn-theme)
+
+(package! afternoon-theme)
+
+;; some (good?) light themes
+(package! ample-theme)        ;; Great dark theme, dull yellow light theme
+(package! anti-zenburn-theme) ;; grey, but less
+(package! plan9-theme)        ;; it's hiiiiigh noon
+(package! flatui-theme)       ;; high-constrast, slightly bluish
